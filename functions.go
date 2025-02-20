8fb4eee8cc66e8d981273048a2939d8ab8b89743
Asadbek Ergashev <asadbek.ergashev.easyto.travel@gmail.com>
2025-02-20 09:09:28 +0500

    implement tools

---
+++ functions.go
@@ -0,0 +1,81 @@
+package ettcodesdk
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/spf13/cast"
+	tgbotapiK "gopkg.in/telegram-bot-api.v4"
+)
+
+type ObjectFunction struct {
+	Cfg    *Config
+	Logger *FaasLogger
+}
+
+func New(cfg *Config) *ObjectFunction {
+	return &ObjectFunction{
+		Cfg:    cfg,
+		Logger: NewLoggerFunction(cfg.FunctionName),
+	}
+}
+
+func (o *ObjectFunction) SendTelegram(text string) error {
+	client := &http.Client{}
+
+	if ContainsLike(Mode, text) {
+		text = strings.Replace(text, "\n", "", -1)
+	} else {
+		text = o.Cfg.FunctionName + " >>> " + time.Now().Format(time.RFC3339) + " >>>>> " + text
+	}
+
+	if o.Cfg.BranchName != "" {
+		text = strings.ToUpper(o.Cfg.BranchName) + " >>> " + text
+	}
+
+	for _, e := range o.Cfg.AccountIds {
+		botUrl := fmt.Sprintf("https://api.telegram.org/bot"+o.Cfg.BotToken+"/sendMessage?chat_id="+e+"&text=%s", text)
+		request, err := http.NewRequest("GET", botUrl, nil)
+		if err != nil {
+			return err
+		}
+
+		resp, err := client.Do(request)
+		if err != nil {
+			return err
+		}
+		resp.Body.Close()
+	}
+
+	return nil
+}
+
+func (o *ObjectFunction) SendTelegramFile(req []byte, filename string) error {
+	err := os.WriteFile(filename, req, 0644)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(filename)
+
+	for _, e := range o.Cfg.AccountIds {
+		bot, err := tgbotapiK.NewBotAPI(o.Cfg.BotToken)
+		if err != nil {
+			return err
+		}
+
+		message := tgbotapiK.NewDocumentUpload(cast.ToInt64(e), filename)
+		_, err = bot.Send(message)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (o *ObjectFunction) Config() *Config {
+	return o.Cfg
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/asadbekGo/go-sdk-tools
+
+go 1.22.0
+
+require (
+	github.com/spf13/cast v1.7.1
+	gopkg.in/telegram-bot-api.v4 v4.6.4
+)
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
